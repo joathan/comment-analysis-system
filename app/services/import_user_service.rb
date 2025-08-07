0212fa0ba9e725a081f0e1b8cd30afded1eec8cc
Joathan Souza <joathanf@gmail.com>
2025-08-06 23:46:02 -0300

    feat(service): ✨ Importação de posts e comentários de usuário
    - Descrição: Cria ImportUserService para importar posts e comentários via API externa, com cobertura de testes VCR.
    - Contexto: Necessidade de automatizar importação de dados de usuários e garantir robustez contra erros de integração.
    - Mudanças:
      - Implementa ImportUserService para buscar e persistir posts/comentários por username
      - Garante idempotência ao reimportar usuários existentes
      - Trata e loga erros de criação de usuário, posts e comentários
      - Adiciona testes automatizados com VCR cobrindo fluxos normais e de falha
      - Gera cassetes VCR para cenários de integração reais e edge cases
    - Impacto: Facilita integração de dados externos e aumenta a confiabilidade e testabilidade do fluxo de importação.

---
+++ app/services/import_user_service.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+class ImportUserService
+  def initialize(username)
+    @username = username
+    @logger = Rails.logger
+  end
+
+  def call
+    user = find_or_create_user
+    import_posts_and_comments(user)
+    # TODO: Se necessário, adicionar jobs para processar comentários ou recalcular métricas
+    # RecalculateGroupMetricsJob.perform_later
+  end
+
+  private
+
+  def find_or_create_user
+    User.find_or_create_by!(username: @username)
+  rescue ActiveRecord::RecordInvalid => e
+    @logger.error("[ImportUserService] Falha ao criar usuário: #{e.message}")
+    raise
+  end
+
+  def import_posts_and_comments(user)
+    posts = JsonPlaceholderAdapter.fetch_user_posts(user.id)
+    posts.each do |post_attrs|
+      post = user.posts.find_or_create_by!(
+        title: post_attrs['title'],
+        body: post_attrs['body']
+      )
+      import_comments_for_post(post)
+    rescue StandardError => e
+      @logger.error("[ImportUserService] Falha no post #{post_attrs['id']}: #{e.message}")
+    end
+  end
+
+  def import_comments_for_post(post)
+    comments = JsonPlaceholderAdapter.fetch_post_comments(post.id)
+    comments.each do |comment_attrs|
+      comment = post.comments.find_or_create_by!(
+        name: comment_attrs['name'],
+        email: comment_attrs['email'],
+        body: comment_attrs['body']
+      )
+      #  TODO: Se necessário, adicionar job para processar o comentário
+      # ProcessCommentJob.perform_later(comment.id)
+    rescue StandardError => e
+      @logger.error("[ImportUserService] Falha no comentário do post #{post.id}: #{e.message}")
+    end
+  end
+end
---
+++ spec/services/import_user_service_spec.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ImportUserService, :vcr do
+  let(:username) { Faker::Internet.unique.username }
+  let(:service) { described_class.new(username) }
+
+  context 'when everything goes normally' do
+    it 'creates the user, their posts, and comments' do
+      expect { service.call }
+        .to change(User, :count).by(1)
+        .and change(Post, :count).by_at_least(1)
+        .and change(Comment, :count).by_at_least(1)
+
+      user = User.find_by(username: username)
+
+      expect(user).to be_present
+      expect(user.posts.count).to be > 0
+      expect(user.posts.first.comments.count).to be > 0
+    end
+  end
+
+  context 'when the user already exists' do
+    let!(:user) { create(:user, username: username) }
+
+    it 'does not create a duplicate user, but imports posts/comments' do
+      expect {
+        service.call
+      }.to change(Post, :count).by_at_least(1)
+      .and change(Comment, :count).by_at_least(1)
+
+      expect(User.where(username: username).count).to eq(1)
+      expect(user.reload.posts.count).to be > 0
+    end
+  end
+
+  context 'when there is an error creating the user' do
+    before do
+      allow(User).to receive(:find_or_create_by!).and_raise(ActiveRecord::RecordInvalid.new(User.new))
+      allow(Rails.logger).to receive(:error)
+    end
+
+    it 'logs the error and propagates the exception' do
+      expect(Rails.logger).to receive(:error).with(/Falha ao criar usuário/)
+      expect { service.call }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
+
+  context 'when there is an error creating a post' do
+    before do
+      allow_any_instance_of(User).to receive_message_chain(:posts, :find_or_create_by!).and_raise(StandardError, 'Post error')
+      allow(Rails.logger).to receive(:error)
+    end
+
+    it 'logs the post error and continues' do
+      expect(Rails.logger).to receive(:error).at_least(:once)
+        .with(a_string_matching(/\[ImportUserService\] Falha no post \d+: Post error/))
+      expect { service.call }.not_to raise_error
+    end
+  end
+
+  context 'when there is an error creating a comment' do
+    before do
+      allow_any_instance_of(Post).to receive_message_chain(:comments, :find_or_create_by!).and_raise(StandardError, 'Comment error')
+      allow(Rails.logger).to receive(:error)
+    end
+
+    it 'logs the comment error and continues' do
+      expect(Rails.logger).to receive(:error).at_least(:once)
+        .with(a_string_matching(/\[ImportUserService\] Falha no comentário do post \d+: Comment error/))
+      expect { service.call }.not_to raise_error
+    end
+  end
+end
